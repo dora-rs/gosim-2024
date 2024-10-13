4467e3381978415a5c32c5d1793847b17dc5f194
Lyon <tgxz2000@gmail.com>
2024-10-13 15:47:47 +0800

    Update enable_service.sh

--- scripts/enable_service.sh
+++ scripts/enable_service.sh
@@ -1,6 +1,21 @@
-BASEDIR=$(dirname $0)
-sudo cp $BASEDIR/dora-daemon.service /etc/systemd/system/dora-daemon.service
+sudo tee /etc/systemd/system/dora-daemon.service << EOF
+Description=Dora Daemon in Conda Environment
+After=network.target
+
+[Service]
+User=HwHiAiUser
+Environment='PATH=$PATH'
+WorkingDirectory=/home/HwHiAiUser
+ExecStart=/bin/bash --login  -c 'source /home/HwHiAiUser/.bashrc && source $CONDA_PREFIX/bin/activate base && dora daemon --inter-daemon-addr 0.0.0.0:20001 --machine-id $(cat /etc/machine-id | head -c 3)'
+Restart=always
+RestartSec=3
+StartLimitInterval=60
+StartLimitBurst=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable dora-daemon.service
-sudo systemctl start dora-daemon.service
+sudo systemctl restart dora-daemon.service
