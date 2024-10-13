a0e5a74d338853da2200186346b784029270a705
haixuanTao <tao.xavier@outlook.com>
2024-10-13 23:26:23 +0200

    Add tag to dora version

--- scripts/install_default.sh
+++ scripts/install_default.sh
@@ -1,2 +1,2 @@
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/dora-rs/dora/main/install.sh | bash
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/dora-rs/dora/main/install.sh | bash -s -- --tag v0.3.7rc0
 source ~/.bashrc
